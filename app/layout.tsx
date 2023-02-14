8456c0e5abeb48841dcf82671558f21f0b7acd4f
Daniel Nguyen <danielnguyen5201@gmail.com>
2023-02-15 05:37:52 +0700

    config project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
+import { ReactNode } from 'react'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
